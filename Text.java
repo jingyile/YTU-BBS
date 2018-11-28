7afd48f8885b5a15b4229ade3dd6c7c2eb73db02
景怡乐 <39609125+jingyile@users.noreply.github.com>
2018-11-28 21:44:39 +0800

    Add files via upload

---
+++ Text.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Text {
+private String id;
+private int num;
+private int userid;
+private String txt;
+public Text()
+{
+	
+}
+public String getTxt() {
+	return txt;
+}
+public void setTxt(String txt) {
+	this.txt = txt;
+}
+public int getUserid() {
+	return userid;
+}
+public void setUserid(int userid) {
+	this.userid = userid;
+}
+public int getNum() {
+	return num;
+}
+public void setNum(int num) {
+	this.num = num;
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+}
